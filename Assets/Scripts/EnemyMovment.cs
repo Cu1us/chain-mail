0d4ba8326ef316a9a0d18cdadf8ae7028a8b1595
SenseiLegacy <danne._boy@hotmail.com>
2024-11-14 09:54:53 +0100

    Sample Scene changes

--- Assets/Scripts/EnemyMovment.cs
+++ Assets/Scripts/EnemyMovment.cs
@@ -8,8 +8,8 @@ public class EnemyMovment : MonoBehaviour
 
     [SerializeField] Transform playerTransform;
     SpriteRenderer spriteRenderer;
-    float movmentSpeed = 5;
-
+    [SerializeField] float movementSpeed = 0.7f;
+    
 
     void Start()
     {
@@ -18,24 +18,20 @@ public class EnemyMovment : MonoBehaviour
 
     void Update()
     {
-        transform.position = Vector2.MoveTowards(transform.position, playerTransform.position, movmentSpeed*Time.deltaTime);
+        transform.position = Vector2.MoveTowards(transform.position, playerTransform.position, movementSpeed*Time.deltaTime);
     }
 
-    void OnTriggerEnter2D()
-    {
-        Stumble();
-    }
     
     public void Stumble()
     {
-        movmentSpeed = 0;
+        movementSpeed = 0;
         spriteRenderer.color = Color.cyan;
         Invoke("StandUp", 2f);
     }
 
     void StandUp()
     {
-        movmentSpeed = 5;
+        movementSpeed = 0.7f;
         spriteRenderer.color = Color.white;
     }
 
