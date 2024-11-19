743b8b939843979535b3f1951e4b5de0345a83fa
Måns Fritiofsson <mans@fritiofson.net>
2024-11-19 14:26:50 +0100

    Started work on chain v2

--- Assets/Scripts/Chain/Chain.cs
+++ Assets/Scripts/Chain/Chain.cs
@@ -6,13 +6,46 @@ using UnityEngine;
 [RequireComponent(typeof(EdgeCollider2D))]
 public class Chain : MonoBehaviour
 {
+    // Exposed fields
+
+    [Header("Status")]
+    [ReadOnlyInspector] public GrabStatus grabStatus;
+
+
+    [Header("Settings")]
+
+
+
+    [Header("Players")]
+    public PlayerMovement PlayerA;
+    public PlayerMovement PlayerB;
+
     [Header("References")]
-    [SerializeField] LineRenderer lineRenderer;
-    [SerializeField] EdgeCollider2D edgeCollider;
+    [ReadOnlyInspector][SerializeField] LineRenderer lineRenderer;
+    [ReadOnlyInspector][SerializeField] EdgeCollider2D edgeCollider;
+
+    // Properties
+    public Vector2 Pivot { get { return Vector2.Lerp(PlayerA.position, PlayerB.position, localPivot); } }
+    public PlayerMovement Grabber { get { return grabStatus switch { GrabStatus.A => PlayerB, GrabStatus.B => PlayerA, _ => null }; } }
+    public PlayerMovement Grabee { get { return grabStatus switch { GrabStatus.A => PlayerB, GrabStatus.B => PlayerA, _ => null }; } }
+
+    // Local variables
+    float localPivot;
+
+
 
     void Reset()
     {
         lineRenderer = GetComponent<LineRenderer>();
         edgeCollider = GetComponent<EdgeCollider2D>();
+    }
+
+
+
+    public enum GrabStatus
+    {
+        A,
+        B,
+        NONE
     }
 }
