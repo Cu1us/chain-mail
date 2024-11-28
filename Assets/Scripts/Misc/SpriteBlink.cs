fd1366f38a8a750c77a90f97360fcd1a6dc652e4
Måns Fritiofsson <mans@fritiofson.net>
2024-11-28 14:31:39 +0100

    Sprite blink functionality

--- Assets/Scripts/Misc/SpriteBlink.cs
+++ Assets/Scripts/Misc/SpriteBlink.cs
@@ -1,18 +1,44 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Renderer))]
 public class SpriteBlink : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [ReadOnlyInspector][SerializeField] new Renderer renderer;
+    float blinkUntil;
+    bool blinking = false;
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (!blinking && Time.time <= blinkUntil)
+        {
+            blinking = true;
+            SetBlinkState(true);
+        }
+        else if (blinking && Time.time > blinkUntil)
+        {
+            blinking = false;
+            SetBlinkState(false);
+        }
+    }
+
+    public void Blink(float duration, bool additive = false)
+    {
+        if (additive)
+            blinkUntil = Mathf.Max(blinkUntil, Time.time) + duration;
+        else
+            blinkUntil = Time.time + duration;
+    }
+
+    public void SetBlinkState(bool blinking)
+    {
+        renderer.material.SetInt("_Blink", blinking ? 1 : 0);
+    }
+
+    void Reset()
+    {
+        renderer = GetComponent<Renderer>();
     }
 }
