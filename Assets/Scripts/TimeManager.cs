d2fea0a0c3b66304e65a7996744c6b4e1f004140
Måns Fritiofsson <mans@fritiofson.net>
2024-11-28 10:13:20 +0100

    Time manager

---
+++ Assets/Scripts/TimeManager.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimeManager : MonoBehaviour
+{
+    public static TimeManager instance;
+    public static float timeScale = 1;
+    static double freezeUntil;
+    public static bool IsFrozen { get { return freezeUntil > Time.unscaledTimeAsDouble; } }
+
+    void Awake()
+    {
+        if (!instance) instance = this;
+    }
+
+    public static void Freeze(float duration)
+    {
+        freezeUntil = System.Math.Max(freezeUntil, Time.unscaledTimeAsDouble) + duration;
+    }
+    public static void Unfreeze()
+    {
+        freezeUntil = default;
+        UpdateTimeScale();
+    }
+    public static void ResetTimeScale()
+    {
+        freezeUntil = default;
+        timeScale = 1;
+        UpdateTimeScale();
+    }
+
+    void Update()
+    {
+        UpdateTimeScale();
+    }
+
+    static void UpdateTimeScale()
+    {
+        float newTimeScale = timeScale;
+        if (Time.unscaledTimeAsDouble < freezeUntil)
+        {
+            timeScale = 0;
+        }
+        Time.timeScale = timeScale;
+    }
+}
