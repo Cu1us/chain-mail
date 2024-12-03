c96aa6ca52062fe1323e93c805c583f369982fd8
SenseiLegacy <danne._boy@hotmail.com>
2024-12-03 10:56:38 +0100

    window trap

---
+++ Assets/WindowTrap.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WindowTrap : MonoBehaviour
+{
+
+    [SerializeField] Sprite brokenWindowSprite;
+    bool isWindowBroken;
+    SpriteRenderer spriteRenderer;
+
+    void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if(!isWindowBroken)
+        {
+            if(other.CompareTag("Enemy"))
+            {
+                float velocity = other.GetComponent<Rigidbody2D>().velocity.sqrMagnitude;
+                if (velocity > 9)
+                {
+                    isWindowBroken = true;
+                    spriteRenderer.sprite = brokenWindowSprite;
+                }
+            }
+        }
+        if(other.CompareTag("Enemy") && isWindowBroken)
+        {
+            other.GetComponent<EnemyMovement>().StateChange(EnemyMovement.EnemyState.STUCK);
+            other.GetComponent<Rigidbody2D>().velocity = new Vector2(0,0);
+            other.GetComponent<Transform>().position = (Vector2)transform.position + new Vector2(0,-4);//(Vector2)other.transform.position + new Vector2(0, 1);
+            other.GetComponent<CapsuleCollider2D>().enabled = false;
+            other.GetComponent<Animator>().SetBool("Trapfall", true);
+        }
+        else if(other.CompareTag("Player"))
+        {
+
+        }
+    }
+}
