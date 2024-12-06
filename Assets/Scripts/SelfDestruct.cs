0596018572ef0ac3c21126f1fe9f25da0620f2f1
Mojkan <elvinhillstrom@gmail.com>
2024-12-06 09:52:34 +0100

    Fixed damage text

--- Assets/Scripts/SelfDestruct.cs
+++ Assets/Scripts/SelfDestruct.cs
@@ -5,13 +5,28 @@ using UnityEngine;
 public class SelfDestruct : MonoBehaviour
 {
     [SerializeField] float selfDestructTimer;
+    [HideInInspector] public Transform targetTransform;
+
     void Start()
     {
         Invoke(nameof(DestroyGameObject), selfDestructTimer);
     }
 
+    void Update()
+    {
+        UpdatePosition();
+    }
+
     void DestroyGameObject()
     {
         Destroy(gameObject);
+    }
+
+    void UpdatePosition()
+    {
+        if (targetTransform != null)
+        {
+            transform.position = targetTransform.position;
+        }
     }
 }
