e6cfc31aa4339e8e60049cc11a6cde243adbfa8e
Måns Fritiofsson <mans@fritiofson.net>
2024-12-10 11:00:39 +0100

    VFX rotation handling

--- Assets/Scripts/VFX.cs
+++ Assets/Scripts/VFX.cs
@@ -4,30 +4,35 @@ using UnityEngine;
 
 public class VFX : MonoBehaviour
 {
-    public static VFX Spawn(VFXType type, Vector2 position, Quaternion rotation)
+    public static VFX Spawn(VFXType type, Vector2 position, Vector2 facingDir)
     {
-        VFX effect = Instantiate(VFXManager.GetPrefab(type), position, rotation);
+        VFX effect = Instantiate(VFXManager.GetPrefab(type), position, Quaternion.identity);
+        effect.facingDir = facingDir;
         return effect;
     }
     public static VFX Spawn(VFXType type, Vector2 position)
     {
-        return Spawn(type, position, Quaternion.identity);
-    }
-    public static VFX Spawn(VFXType type, Vector2 position, Vector2 facingDir)
-    {
-        return Spawn(type, position, Quaternion.LookRotation(facingDir.normalized));
+        return Instantiate(VFXManager.GetPrefab(type), position, Quaternion.identity);
     }
 
     [SerializeField] ParticleSystem[] particleSystems;
     float lifetime;
+    Vector2 facingDir = Vector2.zero;
 
     void Start()
     {
         float duration = 0.25f;
         foreach (ParticleSystem particleSystem in particleSystems)
         {
+            ParticleSystem.MainModule main = particleSystem.main;
+            ParticleSystem.MinMaxCurve startRotation = main.startRotation;
+
+            float rotation = Vector2.SignedAngle(Vector2.right, facingDir) * Mathf.Deg2Rad;
+            if (facingDir != Vector2.zero) startRotation.constant = rotation + startRotation.constant;
+
+            main.startRotation = startRotation;
             particleSystem.Play();
-            duration = Mathf.Max(duration, particleSystem.main.duration);
+            duration = Mathf.Max(duration, main.duration);
         }
         lifetime = duration;
     }
