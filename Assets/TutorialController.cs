b31105078b1b7c2af37a8f2216cc9bc2d976dfc5
Mojkan <elvinhillstrom@gmail.com>
2024-12-12 13:47:25 +0100

    Updated tutorial scene

--- Assets/TutorialController.cs
+++ Assets/TutorialController.cs
@@ -1,5 +1,6 @@
 using DG.Tweening;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class TutorialController : MonoBehaviour
 {
@@ -8,19 +9,32 @@ public class TutorialController : MonoBehaviour
 
     [Header("Reference")]
     [SerializeField] PlayerInputData playerInputData;
+    [SerializeField] Chain chain;
     [SerializeField] SwingableObject player1;
     [SerializeField] SwingableObject rock;
 
-
+    bool animationComplete;
     bool currentStepComplete;
     int currentStep;
 
+    // Step 1
+    bool jButton;
+    bool lButton;
+
+    // Step 2
+    bool player1Swapped;
+    bool rockSwapped;
     void Start()
     {
         PlayTextAnimation();
     }
 
     void Update()
+    {
+        UpdateCurrentStep();
+    }
+
+    void UpdateCurrentStep()
     {
         if (currentStep < tutorialSteps.Length)
         {
@@ -29,6 +43,7 @@ public class TutorialController : MonoBehaviour
             if (currentStepComplete)
             {
                 currentStep++;
+                animationComplete = false;
                 currentStepComplete = false;
                 PlayTextAnimation();
             }
@@ -39,11 +54,11 @@ public class TutorialController : MonoBehaviour
         if (currentStep != 0)
         {
             tutorialSteps[currentStep - 1].GetComponent<RectTransform>().DOAnchorPosX(-2100, 2).SetEase(Ease.InOutQuad).OnComplete(() => 
-                tutorialSteps[currentStep].GetComponent<RectTransform>().DOAnchorPosX(-600, 2).SetEase(Ease.InOutQuad));
+                tutorialSteps[currentStep].GetComponent<RectTransform>().DOAnchorPosX(-800, 2).SetEase(Ease.InOutQuad).OnComplete(() => animationComplete = true));
         }
         else
         {
-            tutorialSteps[currentStep].GetComponent<RectTransform>().DOAnchorPosX(-600, 2).SetEase(Ease.InOutQuad);
+            tutorialSteps[currentStep].GetComponent<RectTransform>().DOAnchorPosX(-800, 2).SetEase(Ease.InOutQuad).OnComplete(() => animationComplete = true);
         }
     }
 
@@ -52,15 +67,56 @@ public class TutorialController : MonoBehaviour
         switch (currentStep)
         {
             case 0:
-                if (Mathf.Abs(player1.swingVelocity) > 0 || Mathf.Abs(rock.swingVelocity) > 0)
-                {
-                    currentStepComplete = true;
-                    Debug.Log("Test");
-                }
+                CheckRotationInput();
                 break;
             case 1:
-
+                CheckAnchorSwitch();
                 break;
+        }
+    }
+
+    void CheckRotationInput()
+    {
+        if (animationComplete)
+        {
+            if (player1.swingVelocity > 4 || rock.swingVelocity > 4)
+            {
+                jButton = true;
+                Transform JButton = tutorialSteps[currentStep].transform.GetChild(1);
+                JButton.gameObject.GetComponent<Image>().color = Color.green;
+
+            }
+            if (player1.swingVelocity < -4 || rock.swingVelocity < -4)
+            {
+                lButton = true;
+                Transform LButton = tutorialSteps[currentStep].transform.GetChild(3);
+                LButton.gameObject.GetComponent<Image>().color = Color.green;
+            }
+            if (jButton && lButton)
+            {
+                currentStepComplete = true;
+            }
+        }
+    }
+
+    void CheckAnchorSwitch()
+    {
+        if (animationComplete)
+        {
+            if (chain.anchorStatus == Chain.AnchorStatus.PLAYER && Mathf.Abs(rock.swingVelocity) > 4)
+            {
+                player1Swapped = true;
+            }
+            if (chain.anchorStatus == Chain.AnchorStatus.ROCK && Mathf.Abs(player1.swingVelocity) > 4)
+            {
+                rockSwapped = true;
+            }
+            if (player1Swapped && rockSwapped)
+            {
+                Transform Button = tutorialSteps[currentStep].transform.GetChild(1);
+                Button.gameObject.GetComponent<Image>().color = Color.green;
+                currentStepComplete = true;
+            }
         }
     }
 }
