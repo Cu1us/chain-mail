c9f3f5edc014c3f93b2a5cf6e87dc693e437e42c
Måns Fritiofsson <mans@fritiofson.net>
2024-12-18 11:44:04 +0100

    Button prompt UI support

--- Assets/Scripts/Misc/ButtonPrompt.cs
+++ Assets/Scripts/Misc/ButtonPrompt.cs
@@ -1,12 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-[RequireComponent(typeof(SpriteRenderer))]
 public class ButtonPrompt : MonoBehaviour
 {
     public bool visible;
     [ReadOnlyInspector][SerializeField] SpriteRenderer spriteRenderer;
+    [ReadOnlyInspector][SerializeField] Image image;
     public float OscillateSpeed;
     [Range(0f, 1f)] public float OscillateStrength;
 
@@ -16,25 +17,32 @@ public class ButtonPrompt : MonoBehaviour
     [SerializeField] Sprite Xbox;
 
     Vector2 defaultScale;
-    void Reset() => spriteRenderer = GetComponent<SpriteRenderer>();
+    void Reset() { TryGetComponent(out spriteRenderer); TryGetComponent(out image); }
     void Awake() => defaultScale = transform.localScale;
 
     void Update()
     {
-        spriteRenderer.enabled = visible;
+        if (spriteRenderer) spriteRenderer.enabled = visible;
+        if (image) image.enabled = visible;
         if (visible)
         {
             if (OscillateSpeed != 0)
             {
-                transform.localScale = defaultScale * (1 + (Mathf.Sin(Time.time * OscillateSpeed) * OscillateStrength));
+                if (spriteRenderer) transform.localScale = defaultScale * (1 + (Mathf.Sin(Time.time * OscillateSpeed) * OscillateStrength));
             }
-            spriteRenderer.sprite = PlayerInputData.inputType switch
+            SetSprite(PlayerInputData.inputType switch
             {
                 PlayerInputData.InputType.Keyboard => Keyboard,
                 PlayerInputData.InputType.Xbox => Xbox,
                 PlayerInputData.InputType.PS4 => PS4,
                 _ => PS4
-            };
+            });
         }
+    }
+
+    void SetSprite(Sprite sprite)
+    {
+        if (spriteRenderer) spriteRenderer.sprite = sprite;
+        if (image) image.sprite = sprite;
     }
 }
