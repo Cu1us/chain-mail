c28d2e14e6e71b28d8078d72d907ac79ebaa248e
Måns Fritiofsson <mans@fritiofson.net>
2025-01-02 13:36:21 +0100

    Swap and smoke trail

--- Assets/Scripts/Player/Rock.cs
+++ Assets/Scripts/Player/Rock.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Rock : SwingableObject
 {
     [SerializeField] TrailRenderer trailRenderer;
+    [SerializeField] ParticleSystem smokeTrail;
     [SerializeField] Animator animator;
 
     Vector2 lastPosition;
@@ -12,6 +13,11 @@ public class Rock : SwingableObject
     protected override void Update()
     {
         if (trailRenderer) trailRenderer.emitting = beingGrabbed;
+        if (smokeTrail)
+        {
+            ParticleSystem.EmissionModule emission = smokeTrail.emission;
+            emission.enabled = beingSwapped;
+        }
 
         float moveSpeed = (lastPosition - (Vector2)transform.position).magnitude / Time.deltaTime;
         animator.SetBool("Moving", moveSpeed > 0.5f);
